d5d0e33004d5c2d4449832230700585132226408
Kosuke Shimofuji <kosuke.shimofuji@gmail.com>
2016-08-01 17:04:24 +0900

    update get_site_hash.go

--- research/get_site_hash.go
+++ research/get_site_hash.go
@@ -3,9 +3,10 @@ package main
 import(
     "fmt"
     "net/http"
-    //"regexp"
+    "regexp"
     "crypto/sha256"
     "io/ioutil"
+    "strings"
 )
 
 func main(){
@@ -23,6 +24,19 @@ func main(){
         defer resp.Body.Close()
         byteArray, _ := ioutil.ReadAll(resp.Body)
         html := string(byteArray)
+
+        //HTMLタグを全て小文字に変換します
+        re, _ := regexp.Compile(`\<[\S\s]+?\>`)
+        html = re.ReplaceAllStringFunc(html, strings.ToLower)
+
+        //<script>タグを除去
+        re, _ = regexp.Compile(`\<script[\S\s]+?\</script\>`)
+        html = re.ReplaceAllString(html, "")
+
+        //コメントタグを除去
+        re, _ = regexp.Compile(`\<\!-\-[\s\S]*?\-\-\>`)
+        html = re.ReplaceAllString(html, "")
+
         hash :=  fmt.Sprintf("%x", sha256.Sum256([]byte(html)))
         fmt.Printf("%s : %s\n", url, hash)
     }
